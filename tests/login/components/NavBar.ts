76697af989aa22f900374f28f2d759015574e95b
jameel98 <jameelmograbi@gmail.com>
2023-11-29 20:52:22 +0200

    add base-page & components & navbar

---
+++ tests/login/components/NavBar.ts
@@ -0,0 +1,40 @@
+import { Locator, Page } from "@playwright/test";
+import { BaseComponent } from "./BaseComponent";
+
+export class NavBar extends BaseComponent {
+  private readonly JUST_LANDED_LOC =
+    "//div[@class='item_1lit']//a[contains(string(),'JUST LANDED')]";
+  private readonly SEARCH_LOC =
+    "//button[@data-test-id='qa-header-search-button']";
+  private readonly CART_LOC = "//a[@data-test-id='qa-link-minicart']";
+  private readonly FAVOURITE_LIST = "//a[@data-test-id='qa-link-wishlist']";
+
+  private justLandedBtn: Locator;
+  private searchBtn: Locator;
+  private cartBtn: Locator;
+  private favouriteListBtn: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.justLandedBtn = page.locator(this.JUST_LANDED_LOC);
+    this.searchBtn = page.locator(this.SEARCH_LOC);
+    this.cartBtn = page.locator(this.CART_LOC);
+    this.favouriteListBtn = page.locator(this.FAVOURITE_LIST);
+  }
+
+  async clickJustLanded() {
+    await this.justLandedBtn.click();
+  }
+
+  async clickSearch() {
+    await this.searchBtn.click();
+  }
+
+  async clickCart() {
+    await this.cartBtn.click();
+  }
+
+  async clickFavourite() {
+    await this.favouriteListBtn.click();
+  }
+}
