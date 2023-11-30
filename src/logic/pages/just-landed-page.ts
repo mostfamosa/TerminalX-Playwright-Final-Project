60e5ce44d546f113f3a8ddac11f717fa2388eefd
jameel98 <jameelmograbi@gmail.com>
2023-11-30 17:46:17 +0200

    added Just Landed Page

---
+++ src/logic/pages/just-landed-page.ts
@@ -0,0 +1,29 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./base-page";
+
+export class justLanded extends BasePage {
+  private readonly ITEM_IN_LIST = (index: number) => {
+    return `//[@class="product-list_yyTm"]//li[${index}]`;
+  };
+
+  private readonly ADD_TO_FAVOURITE = (index: number) => {
+    return `(//div[@class='btn-quick_3Pv7 btn-my_list_2EOz'])[${index}]`;
+  };
+
+  private itemInList: Locator;
+  private addToFavourite: Locator;
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async clickOnRandomItem(index: number) {
+    this.itemInList = this.page.locator(this.ITEM_IN_LIST(index));
+    await this.itemInList.click();
+  }
+
+  async clickOnAddToFavourite(index: number) {
+    this.addToFavourite = this.page.locator(this.ADD_TO_FAVOURITE(index));
+    await this.itemInList.click();
+  }
+}
