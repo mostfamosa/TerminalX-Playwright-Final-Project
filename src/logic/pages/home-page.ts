005fc4a24c449767eef92a1cc86f63abc23d88af
nawras kabha <knawras17@gmail.com>
2023-11-30 18:12:06 +0200

    added home page

---
+++ src/logic/pages/home-page.ts
@@ -0,0 +1,25 @@
+import { BasePage } from "./base-page";
+import { Page } from '@playwright/test';
+import { NavBar } from "../components/nav-bar";
+
+export class HomePage extends BasePage {
+
+  private navBar: NavBar;
+
+  constructor(page: Page) {
+    super(page);
+    this.navBar = new NavBar(page);
+  }
+
+  async clickJustLandedFromNavBar() {
+    await this.navBar.clickJustLanded();
+  }
+
+  async clickSearchFromNavBar() {
+    await this.navBar.clickSearch();
+  }
+
+  async clickCartFromNavBar() {
+    await this.navBar.clickCart();
+  }
+}
