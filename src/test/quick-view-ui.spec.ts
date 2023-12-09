4bf9fff9c41271c1ef242b5d4ad645cbefeb9e83
jameel98 <jameelmograbi@gmail.com>
2023-12-09 11:21:57 +0200

    fix bug in item page

--- src/test/quick-view-ui.spec.ts
+++ src/test/quick-view-ui.spec.ts
@@ -1,15 +1,11 @@
 import { test, expect, Browser, chromium } from '@playwright/test';
 import { ItemPage } from "../logic/pages/item-page";
 import { urls } from '../config/pages-urls.json';
-import { it } from 'node:test';
-import { CartPage } from '../logic/pages/cart-page';
 
 test.describe('item details Validations Suite', () => {
     let browser: Browser;
     let item: ItemPage;
-
-
-
+    let index: number
     test.beforeAll(async () => {
         browser = await chromium.launch();
     });
@@ -17,7 +13,8 @@ test.describe('item details Validations Suite', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto(urls.just_landed_page);
         item = new ItemPage(page);
-        await item.clickrRandomItem(3);
+        index = 3
+        await item.clickrRandomItem(index);
     });
 
     test.afterEach(async ({ page }) => {
@@ -28,60 +25,48 @@ test.describe('item details Validations Suite', () => {
     });
 
 
-    test('extract the item name before adding ot ->add random item ->  Validate the item name match', async ({ page }) => {
+    test('extract the item name before adding ot ->add random item ->  Validate the item name match', async () => {
         const details = await item.getItemDetails();
-        const itemName = await item.getRandomItemName(3);
+        const itemName = await item.getRandomItemName(index);
         expect(itemName).toEqual(details.name);
     });
-    test('extract the item label from the list of the item ->validate the same lable appears when over veiwing the item ', async ({ page }) => {
-        const label = await item.nameTag(3);
+    test('extract the item label from the list of the item ->validate the same lable appears when over veiwing the item ', async () => {
+        const label = await item.nameTag(index);
         const detailsLabel = await item.getItemDetails();
         expect(label).toEqual(detailsLabel.tag);
     });
 
-    test('extract the item final price from the list of the item ->validate the same price appears when over veiwing the item ', async ({ page }) => {
-        const price = await item.finalPrice(3);
+    test('extract the item final price from the list of the item ->validate the same price appears when over veiwing the item ', async () => {
+        const price = await item.finalPrice(index);
         const detailsPrice = await item.getItemDetails();
-        console.log(price);
-        console.log(detailsPrice.finalprice);
         expect(price).toEqual(detailsPrice.finalprice);
     });
-    test('extract the item actual price from the list of the item ->validate the same price appears when over veiwing the item ', async ({ page }) => {
-        const price = await item.actuallPrice(3);
+    test('extract the item actual price from the list of the item ->validate the same price appears when over veiwing the item ', async () => {
+        const price = await item.actuallPrice(index);
         const detailsPrice = await item.getItemDetails();
-        console.log(price);
-        console.log(detailsPrice.actualprice);
         expect(price).toEqual(detailsPrice.actualprice);
     });
-    test('extract the item sale precentage from the list of the item ->validate the same precent appears when over veiwing the item ', async ({ page }) => {
-        const sale = await item.salePrecent(3);
+    test('extract the item sale precentage from the list of the item ->validate the same precent appears when over veiwing the item ', async () => {
+        const sale = await item.salePrecent(index);
         const detailsSale = await item.getItemDetails();
-        console.log(sale);
-        console.log(detailsSale.sale);
         expect(sale).toEqual(detailsSale.sale);
     });
 
-    test('choose a random color -> check if the color name matching the choosen one ', async ({ page }) => {
+    test('choose a random color -> check if the color name matching the choosen one ', async () => {
         const detailsColor = await item.getItemDetails();
         const Colortitle = await item.colorTitle();
-        console.log(Colortitle);
-        console.log(detailsColor.color);
         expect(detailsColor.color).toContain(Colortitle);
     });
 
-    test('extract the item img url  ->go to over view -> validate the same url picture appears ', async ({ page }) => {
-        const Url = await item.itemImage(3);
+    test('extract the item img url  ->go to over view -> validate the same url picture appears ', async () => {
+        const Url = await item.itemImage(index);
         const detailsUrl = await item.getItemDetails();
-        console.log(Url);
-        console.log(detailsUrl.itemUrl);
         expect(Url).toEqual(detailsUrl.itemUrl);
     });
-///
-    test('extract the item brand name ->go to over view -> validate the same brand name appears ', async ({ page }) => {
-        const brand = await item.brandName(3);
+
+    test('extract the item brand name ->go to over view -> validate the same brand name appears ', async () => {
+        const brand = await item.brandName(index);
         const detailsBrand = await item.getItemDetails();
-        console.log(brand);
-        console.log(detailsBrand.Itembrand);
         expect(brand).toEqual(detailsBrand.Itembrand);
     });
 
